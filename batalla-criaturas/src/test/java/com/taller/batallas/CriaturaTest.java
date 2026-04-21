7a46431d1a4d1dc7b4f11a9b09c336f48d5ce14f
Juanfelipe0328 <jbustamante@estudiante.uniajc.edu.co>
2026-04-20 23:58:12 -0500

    Agregando el test criaturas

---
+++ batalla-criaturas/src/test/java/com/taller/batallas/CriaturaTest.java
@@ -0,0 +1,29 @@
+package com.taller.batallas;
+
+import com.taller.batallas.criaturas.Guerrero;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CriaturaTest {
+
+    @Test
+    public void pruebaEstaViva() {
+        Guerrero guerrero = new Guerrero("Prueba", 50, 10, "espada");
+        assertTrue(guerrero.estaViva());
+    }
+
+    @Test
+    public void pruebaDefenderReduceSalud() {
+        Guerrero guerrero = new Guerrero("Prueba", 50, 10, "espada");
+        guerrero.defender(20);
+        assertEquals(30, guerrero.getSalud());
+    }
+
+    @Test
+    public void pruebaCriaturaMuere() {
+        Guerrero guerrero = new Guerrero("Prueba", 30, 10, "espada");
+        guerrero.defender(40);
+        assertFalse(guerrero.estaViva());
+    }
+}
