b3619cf118b5cff64785822724d977c8f41d7c8b
Juanfelipe0328 <jbustamante@estudiante.uniajc.edu.co>
2026-04-20 21:01:37 -0500

    creando estructura maven

---
+++ batalla-criaturas/src/main/java/com/taller/batallas/App.java
@@ -0,0 +1,13 @@
+package com.taller.batallas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
