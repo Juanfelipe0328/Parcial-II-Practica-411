1d81e06de0386debb7692cdc7ec9eda6507b1734
Juanfelipe0328 <jbustamante@estudiante.uniajc.edu.co>
2026-04-20 23:42:45 -0500

    Agregando clase de armas

---
+++ batalla-criaturas/src/main/java/com/taller/batallas/armas/Armas.java
@@ -0,0 +1,27 @@
+package com.taller.batallas.armas;
+
+import com.taller.batallas.criaturas.Criatura;
+
+public class Arma {
+
+    private String nombre;
+    private int danioAdicional;
+
+    public Arma(String nombre, int danioAdicional) {
+        this.nombre = nombre;
+        this.danioAdicional = danioAdicional;
+    }
+
+    public void atacarConArma(Criatura objetivo) {
+        System.out.println("El arma " + nombre + " causa " + danioAdicional + " puntos de daño adicional.");
+        objetivo.defender(danioAdicional);
+    }
+
+    public int getDanioAdicional() {
+        return danioAdicional;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
